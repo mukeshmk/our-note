7d2bcd2327418b98a57aa82810476e3d223ed62d
Mukesh <amukesh.mk@gmail.com>
2015-11-09 21:32:29 +0530

    create note php - start
    create note php - start

---
+++ create/cn_php.php
@@ -0,0 +1,42 @@
+<?php
+	require_once('../login/auth.php');
+?>
+<html>
+	<head>
+		<title>Disply</title>
+		<meta charset='utf-8'>
+   		<meta name='viewport' content='width=device-width, initial-scale=1'>
+  		<link rel='stylesheet' href='../bootstrap-3.3.5-dist/css/bootstrap.min.css'>
+		<script src='../bootstrap-3.3.5-dist/jquery.min.js'></script>
+  		<script src='../bootstrap-3.3.5-dist/js/bootstrap.min.js'></script>
+	</head>
+	
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  		<div class="container-fluid">
+    		<div class="navbar-header">
+      			<a class="navbar-brand" href="" target="">Our Note</a>
+    		</div>
+    		<div>
+      			<ul class="nav navbar-nav">
+        			<li><a href="../home.php">Home</a></li>
+					<li class="active" class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+							Create <span class="caret"></span>	
+						</a>
+						<ul class="dropdown-menu">
+            				<li><a href="create_note.php">Note</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="create_chk.php">Check Box</a></li>
+          				</ul>
+					</li>
+        			<li><a href="../display/disp.php">Display</a></li>
+					<li><a href="../search/search.php">Search</a></li>
+					<li><a href="../update/update.php">Update</a></li>
+					<li><a href="../delete/del_disp.php">Delete</a></li>
+      			</ul>
+				<ul class="nav navbar-nav navbar-right">
+        			<li><a href='../index.php'><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
+      			</ul>
+    		</div>
+  		</div>
+	</nav>
