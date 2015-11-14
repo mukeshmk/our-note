2ca443d653509b76f73d9e194582067adb9ac8ff
Mukesh <amukesh.mk@gmail.com>
2015-11-14 23:39:17 +0530

    Display Color Add
    Display Color Add

--- display/del.php
+++ display/del.php
@@ -2,7 +2,7 @@
 
     require_once('../login/auth.php');
 
-  	$connect=mysqli_connect("localhost","root","");
+	$connect=mysqli_connect("localhost","root","");
     if (!$connect) 
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -15,17 +15,14 @@
             echo "Unable to locate database."; 
         }
     }
-
     echo $_GET['id'];
     echo $_SESSION['SESS_MEMBER_ID'];
-    
-	$result1 = mysqli_query($connect, "DELETE FROM Date_N where note_id =".$_GET['id']." and user_id = ".$_SESSION['SESS_MEMBER_ID']);
+    $result1 = mysqli_query($connect, "DELETE FROM Date_N where note_id =".$_GET['id']." and user_id = ".$_SESSION['SESS_MEMBER_ID']);
     $result2 = mysqli_query($connect, "DELETE FROM Note where note_id =".$_GET['id']." and user_id = ".$_SESSION['SESS_MEMBER_ID']);
 
     if (!$result1 || !$result2)  
     {  
     	echo "Error fetching data: " . mysqli_error($connect);  
     }  
-
-    require('disp.php');    
+    require('disp.php');
 ?>
--- display/disp.php
+++ display/disp.php
@@ -1,6 +1,6 @@
 
 <?php 
-require_once('../login/auth.php');
+	require_once('../login/auth.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,8 +13,6 @@ require_once('../login/auth.php');
   		<script src="../bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
   		<link href="box.css" rel="stylesheet">
 	</head>
-    
-	
 	<nav class="navbar navbar-inverse navbar-fixed-top">
   		<div class="container-fluid">
     		<div class="navbar-header">
@@ -36,76 +34,106 @@ require_once('../login/auth.php');
         			<li class="active"><a href="disp.php">Display</a></li>
 					<li><a href="../search/search.php">Search</a></li>
 					<li><a href="../update/update.php">Update</a></li>
-					<li><a href="../delete/delete.php">Delete</a></li>
+					<li><a href="disp.php">Delete</a></li>
       			</ul>
 				<ul class="nav navbar-nav navbar-right">
-        			<li><a href='index.php'><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
+        			<li><a href='../index.php'><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
       			</ul>
     		</div>
   		</div>
 	</nav>
 
     <?php
-    $connect=mysqli_connect("localhost","root","");
+	echo'<br><br><br>';
+	$connect=mysqli_connect("localhost","root","");
     if (!$connect) 
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     else
 	{
-		mysqli_set_charset($connect, 'utf8');
+        mysqli_set_charset($connect, 'utf8');
         if (!mysqli_select_db($connect, 'dbms_pro')) 
         { 
             echo "Unable to locate database."; 
         }
     }
 
-
-    $result = mysqli_query($connect, "SELECT title, notes, note_id FROM Note where user_id = '".$_SESSION['SESS_MEMBER_ID']."'");
-
+    $result = mysqli_query($connect, "SELECT title, notes, note_id, clr_code FROM Note where user_id = '".$_SESSION['SESS_MEMBER_ID']."'");
     if (!$result)  
     {  
     	echo "Error fetching data: " . mysqli_error($connect);  
-    }  
-
-    while ($row = mysqli_fetch_array($result))  
-    {  
-    	$titles[] = $row['title'];
-    	$notes[] = $row['notes'];  
-    	$note_id[] = $row['note_id'];
-    }  
-?>
+    }
+	if(mysqli_num_rows($result) > 0) 
+    {
+		while ($row = mysqli_fetch_array($result))
+    	{
+    		$titles[] = $row['title'];
+    		$notes[] = $row['notes'];  
+    		$note_id[] = $row['note_id'];
+    		$color_code[] = $row['clr_code'];
+    	}
+	
+    ?>
 	<br><br><br>
 	<body>
 
-		<div class="container">
+		<div class="container">		
 			<div class="container">
-	           <div class="row">
-				   <div class="row">
-                    <?php $x=0; foreach ($notes as $note): ?>
-                    <?php echo "<div class=\"col-md-4 text-center\"><div class=\"box\"><div class=\"box-content\">
-                                <h1 class=\"tag-title\">";?>
-                                <?php echo $titles[$x]; ?>
-                                <?php echo "</h1><hr />"; ?>
-                                <?php echo $note; ?>
-
-                                <?php echo "<br />"; ?>
-                                <?php echo "<br />
-                                
+	          <div class="row">
+               	<div class="row">
+                <?php 
+						$x=0; 
+						foreach ($notes as $note):
+                    		echo "<div class=\"col-md-4 text-center\" ><div class=\"box\" style=\"background-color:";
+                    	switch ($color_code[$x]) 
+						{
+                        	case 'Red':
+                            	echo '#FF3300';
+                                break;
+                            case 'Blue':
+                            	echo '#66CCFF';
+                                break;
+                            case 'Green':
+                            	echo '#00FF66';
+                                break;
+                           	case 'Yellow':
+                            	echo '#FFFF00';
+                                break;
+                            case 'Violet':
+                            	echo '#9900FF';
+                                break;
+                            default:
+                            	echo '#FFFFCC';
+                                break;
+                      	}
+                        echo "\"><div class=\"box-content\">
+                                <h1 class=\"tag-title\">";
+                        echo $titles[$x];
+                        echo "</h1><hr />";
+                        echo $note;
+                        echo "<br />";
+                        echo "<br />
                                 <p>                          
-                                <a href=\"../update/update.php?id=".$note_id[$x]."\" class=\"btn btn-primary\">Edit</a> 
-                                <a href=\"del.php?id=".$note_id[$x]."\""; ?>
-                                
-                                <?php echo " class=\"btn btn-primary\">Delete</a>
+                                <a href=\"ppc.html\" class=\"btn btn-primary\">Edit</a> 
+                                <a href=\"del.php?id=".$note_id[$x]."\"";
+                                echo " class=\"btn btn-primary\">Delete</a>
                                 <a href=\"disp.php\" class=\"btn btn-primary\">Learn more</a></p>
                             </div>
                         </div>
-                    </div>"; ?>
-                    <?php $x=$x+1; endforeach;?>
+                    </div>";
+
+                  	$x=$x+1; endforeach; 
+	}
+	else
+	{
+		echo'<h1 align="center">NO Notes to Display !!</h1>';
+	}
+				?>
+                    
                 </div>           
             </div>
 	   </div>
     </div>
-	
 	</body>
 </html>
