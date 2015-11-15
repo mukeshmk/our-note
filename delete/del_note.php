1ce270bd3fb762ef2b8f58cdae6962acff767ca2
Mukesh <amukesh.mk@gmail.com>
2015-11-15 15:51:58 +0530

    del - nav bar add
    del - nav bar addition

--- delete/del_note.php
+++ delete/del_note.php
@@ -1,7 +1,47 @@
 <?php 
-
     require_once('../login/auth.php');
-
+?>
+<html>
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="../bootstrap-3.3.5-dist/css/bootstrap.min.css">
+		<script src="../bootstrap-3.3.5-dist/jquery.min.js"></script>
+  		<script src="../bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+  		<link href="box.css" rel="stylesheet">
+	</head>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  		<div class="container-fluid">
+    		<div class="navbar-header">
+      			<a class="navbar-brand" href="" target="">Our Note</a>
+    		</div>
+    		<div>
+      			<ul class="nav navbar-nav">
+        			<li><a href="../home.php">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+							Create <span class="caret"></span>	
+						</a>
+						<ul class="dropdown-menu">
+            				<li><a href="../create/create_note.php">Note</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="../create/create_chk.php">Check Box</a></li>
+          				</ul>
+					</li>
+        			<li class="active"><a href="disp.php">Display</a></li>
+					<li><a href="../search/search.php">Search</a></li>
+					<li><a href="../update/update.php">Update</a></li>
+					<li><a href="disp.php">Delete</a></li>
+      			</ul>
+				<ul class="nav navbar-nav navbar-right">
+        			<li><a href='../index.php'><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
+      			</ul>
+    		</div>
+  		</div>
+	</nav>
+</html>
+<?php
 	$connect=mysqli_connect("localhost","root","");
     if (!$connect) 
     {
@@ -15,17 +55,21 @@
             echo "Unable to locate database."; 
         }
     }
-    echo $_GET['id'];
-    echo $_SESSION['SESS_MEMBER_ID'];
-    $result1 = mysqli_query($connect, "DELETE FROM Date_N where note_id =".$_GET['id']." and user_id = ".$_SESSION['SESS_MEMBER_ID']);
-    $result2 = mysqli_query($connect, "DELETE FROM Note where note_id =".$_GET['id']." and user_id = ".$_SESSION['SESS_MEMBER_ID']);
-
-    if (!$result1 || !$result2)  
+ 
+	$result1 = mysqli_query($connect, "DELETE FROM Date_N where note_id=".$_GET['id']." and user_id=".$_SESSION['SESS_MEMBER_ID']);
+	if (!$result1)  
     {  
-    	echo "Error fetching data: " . mysqli_error($connect);  
-    }  
+    	echo "Error fetching data 1: " . mysqli_error($connect);  
+    }    
+	
+
+	$result2 = mysqli_query($connect, "DELETE FROM Note where note_id=".$_GET['id']." and user_id=".$_SESSION['SESS_MEMBER_ID']);
+	if (!$result2)  
+    {  
+    	echo "Error fetching data 2: " . mysqli_error($connect);  
+    }
 ?>
 <html>
 	<meta http-equiv="refresh" content="0; URL=../display/disp.php">
-	<meta name="keywords" content="automatic redirection"
+	<meta name="keywords" content="automatic redirection">
 </html>
